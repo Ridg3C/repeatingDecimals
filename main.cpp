74fc8fd346ce0fe96e602a8d79cc874845b0e69c
Ridg3C <checinskibartlomiej@gmail.com>
2020-11-27 15:19:34 +0100

    First commit

--- main.cpp
+++ main.cpp
@@ -1,151 +1,77 @@
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 #include <sstream>
+#include <string>
 
 using namespace std;
-string constantString;
-
-string rep;
-string repHelper;
-string a1;
-string a2;
-string numeratorString;
-string denominatorString;
-int notDot=0;
-int decimals;
-int numberOfDigits=0;
-float constantFloat;
-float a1F;
-float a2F;
-float q;
-float numerator;
-float denominator;
 
 int main()
 {
-    cout<<"Input constant: ";
-    cin>>constantString;
-    cout<<"Input number which will repeat: ";
-    cin>>rep;
-    repHelper=rep;
+float x, r;
+cout<<"Input the first part of Your number: "<<endl;
+cin>>x;
+cout<<"Input the repeating part of Your number: "<<endl;
+cin>>r;
 
-    for (int i=0; i<constantString.size(); i++)
+string s;
+string t;
+stringstream out;
+out << x;
+s=out.str();
+
+t=s.substr(s.find(".")+1);
+int l=t.length();
+size_t d=s.find(".");
+if (d==string::npos)
+{
+    l=0;
+}
+
+int cnt=0;
+cnt=1+floor(log10(r));
+if (cnt==1)
+{
+    for (int i=1; i<=cnt+1; i++)
     {
-        if (constantString[i]!='.')
-            notDot++;
-        else
-            break;
+        x+=r/(pow(10,i+l));
     }
+}
+else
+{
+    x=x+(r/(pow(10,l+cnt)));
 
-    for (int i=0; i<constantString.size(); i++)
-    {
-        if (constantString[i]=='.')
+}
+
+float hx;
+float denominator;
+
+if (cnt==1)
+{
+    hx=x*100;
+for (int i=1; i<=cnt+1; i++)
         {
-            decimals=constantString.size()-notDot-1;
-            break;
+            hx+=(r/(pow(10,i+l)));
         }
-        else
-            decimals=constantString.size()-notDot;
-    }
+    denominator=99;
+}
+else
+{
+    hx=x*pow(10,cnt);
+    hx+=(r/(pow(10,l+cnt)));
+    denominator=pow(10,cnt)-1;
+}
+float numerator=hx-x;
 
+cout<<"In order to obtain Your desired number divide "<<numerator<<" by "<<denominator<<" if c++ messed up numbers, else divide: ."<<endl;
 
-
-    for (int i=0; i<rep.size(); i++)
-    {
-        numberOfDigits++;
-    }
-
-    if (constantString[0]=='-')
-    {
-        a1[0]='-';
-        a1[1]='0';
-        a1[2]='.';
-        a2[0]='-';
-        a2[1]='0';
-        a2[2]='.';
-
-        for (int i=3; i<decimals+3; i++)
-        {
-            a1[i]='0';
-        }
-        for (int i=decimals+3; i<decimals+3+numberOfDigits; i++)
-        {
-            a1[i]=rep[i-3-decimals];
-        }
-        for (int i=3; i<decimals+3+numberOfDigits; i++)
-        {
-            a2[i]='0';
-        }
-        for (int i=decimals+3+numberOfDigits; i<decimals+3+numberOfDigits*2; i++)
-        {
-            a2[i]=rep[i-3-decimals-numberOfDigits];
-        }
-    }
-    else
-    {
-        a1[0]='0';
-        a1[1]='.';
-        a2[0]='0';
-        a2[1]='.';
-
-        for (int i=2; i<decimals+2; i++)
-        {
-            a1[i]='0';
-        }
-        for (int i=decimals+2; i<decimals+2+numberOfDigits; i++)
-        {
-            a1[i]=rep[i-2-decimals];
-        }
-        for (int i=2; i<decimals+2+numberOfDigits; i++)
-        {
-            a2[i]='0';
-        }
-        for (int i=decimals+2+numberOfDigits; i<decimals+2+numberOfDigits*2; i++)
-        {
-            a2[i]=rep[i-2-decimals-numberOfDigits];
-        }
-    }
-
-    cout<<"Your number: "<<constantString;
-    if (decimals<=0)
-        cout<<".";
-
-    cout<<rep<<rep<<rep<<"..."<<endl;
-
-    a1F = stof(a1);
-    a2F = stof(a2);
-    constantFloat = stof(constantString);
-
-    q=a2F/a1F;
-
-    denominator=(1-q)*pow(10,numberOfDigits);
-    numerator=a1F*pow(10,numberOfDigits) + constantFloat*denominator;
-
-    if (int(numerator)!=numerator)
-    {
-        numerator=numerator*10;
-        denominator=denominator*10;
-    }
-
-    if (numerator>=0)
-        numerator+=0.5;
-    else
-        numerator-=0.5;
-
-
-    int numeratorInt = static_cast<int>(numerator);
-    int denominatorInt = static_cast<int>(denominator);
-
-
-    while (numeratorInt%10==0 && denominatorInt%10==0)
-    {
-        numeratorInt=numeratorInt/10;
-        denominatorInt=denominatorInt/10;
-    }
-
-    cout<<"In order to obtain your desired number divide these numbers: "<<endl;
-    cout<<"numerator: "<<numeratorInt<<endl;
-    cout<<"denominator: "<<denominatorInt<<endl;
-    return 0;
+if (cnt>=2 || t.length()>=2)
+{
+    while (int(numerator)!=numerator)
+{
+    numerator*=10;
+    denominator*=10;
+}
+}
+cout<<numerator<<" by "<<denominator<<".";
+return 0;
 }
